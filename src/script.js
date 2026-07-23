946a0e30d30cae40349c73b472b8a08413d7b9bd
mabas <johnmabas1@gmail.com>
2026-07-23 11:12:25 +0100

    add blog page

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("menuBtn");
 const menu = document.getElementById("mobileMenu");
 const iconOpen = document.getElementById("iconOpen");
 const iconClose = document.getElementById("iconClose");
+
 btn.addEventListener("click", () => {
   const isHidden = menu.classList.toggle("hidden");
   btn.setAttribute("aria-expanded", String(!isHidden));
@@ -10,24 +11,3 @@ btn.addEventListener("click", () => {
 });
 
 
- async function fetchUsers() {
-    try {
-        const response = await fetch("https://dummyjson.com/posts");
-        const data = await response.json();
-
-        return data.posts.map(post => post.body);
-    } catch (e) {
-        console.log(e.message);
-        return [];
-    }
-}
-
-fetchUsers().then(posts => {
-    const speaker = document.querySelectorAll(".post");
-
-    speaker.forEach((element, index) => {
-        if (posts[index]) {
-            element.innerText = posts[index];
-        }
-    });
-});
